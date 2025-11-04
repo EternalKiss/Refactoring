ab138dd99c9c48cc6b16cfc2a5b3a65de8510437
KISS <kalinichenka2002@mail.ru>
2025-11-04 19:11:52 +0500

    fix

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public void Initialization(Vector3 direction, float speed)
+    public void Initialization(Vector3 direction, float bulletSpeed)
     {
         transform.up = direction;
-        GetComponent<Rigidbody>().linearVelocity = direction * speed;
+        GetComponent<Rigidbody>().linearVelocity = direction * bulletSpeed;
     }
 }
